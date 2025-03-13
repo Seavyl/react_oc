1579643b44a00c901340fa3a902151ca7acb55c5
lombardi <charlene.lombardi@ik.me>
2025-03-13 14:34:11 +0100

    ok ça marche

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -1,13 +1,14 @@
 import Banner from './Banner'
 import logo from '../assets/logo.png'
 // import Cart from './Cart'
-	import ShoppingList from './ShoppingList'
+import ShoppingList from './ShoppingList'
+import PlantItem from './PlantItem'
 
 
 function App() {
 	return (
 		<div>
-			
+			<PlantItem />
 			<Banner>
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
