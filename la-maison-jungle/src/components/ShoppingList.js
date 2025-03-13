7c5bfe84a70dc5f315485f5cd55ca5b8c0a2525a
lombardi <charlene.lombardi@ik.me>
2025-03-13 14:32:21 +0100

    weird

--- la-maison-jungle/src/components/ShoppingList.js
+++ la-maison-jungle/src/components/ShoppingList.js
@@ -19,11 +19,12 @@ function ShoppingList() {
 				{plantList.map((plant) => (
 					<li key={plant.id} className='lmj-plant-item'>
 						{plant.name}
+						{plant.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
 					</li>
 				))}
 			</ul>
 		</div>
-	)
+			)
 }
 
 export default ShoppingList
